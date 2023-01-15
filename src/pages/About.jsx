586f6d8e8917bace2a0c70255e2a81863629c1f6
Victor-Olufade <olufadevictor97@gmail>
2023-01-15 16:20:39 +0100

    create about, home, notfound pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1 className="text-6xl mb4">
+        <p className='mb-4 text-2xl font-light'>
+        This is a Github finder application where you can find just  <strong>anyone</strong> on registered on Github.
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+      <p className='text-lg text-gray-400 py-1'>
+       You will find it interesting
+      </p>
+        </h1>
+    </div>
+  )
+}
+
+export default About
